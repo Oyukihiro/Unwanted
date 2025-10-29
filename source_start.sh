193f84a014c7e2fc00e430322e39ac531fea73f0
Kimmy/LITT <156049186+Oyukihiro@users.noreply.github.com>
2025-10-29 10:54:43 -0300

    fix source_start.sh

--- source_start.sh
+++ source_start.sh
@@ -1,2 +1,40 @@
-429: Too Many Requests
-For more on scraping GitHub and how it may affect your rights, please review our Terms of Service (https://docs.github.com/en/site-policy/github-terms/github-terms-of-service).
+#!/bin/bash --utf8
+
+export PYTHONIOENCODING=utf8
+
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
+
+if [[ $OSTYPE == "msys" ]]; then
+  VENV_PATH=venv/Scripts/activate
+else
+  VENV_PATH=venv/bin/activate
+fi
+
+if [ ! -d "venv" ]; then
+  if [ -x "$(command -v py)" ]; then
+    py -3 -m venv venv
+  else
+    python3 -m venv venv
+  fi
+
+  if [ ! -d "venv" ]; then
+    echo "The venv folder was not created! Check if you installed Python correctly (and that it is configured in the PATH/env)."
+    sleep 45
+    exit 1
+  fi
+
+  source $VENV_PATH
+  pip install -r requirements.txt
+else
+  source $VENV_PATH
+fi
+
+echo "Iniciando bot (Verifique se o mesmo está online)..."
+
+#mkdir -p ./.logs
+
+#touch "./.logs/run.log"
+
+python main.py #2>&1 | tee ./.logs/run.log
+
+sleep 120s
